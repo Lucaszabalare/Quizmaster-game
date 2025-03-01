bc5e5a08073623c08e49892c948fcac275dede62
Lucaszabalare <nellymarac@gmail.com>
2025-03-01 11:27:51 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -31,3 +31,31 @@ answer_box2.move_ip(370,270)
 answer_box3.move_ip(20,450)
 answer_box4.move_ip(370,450)
 timer_box.move_ip(700,100)
+skip_box.move_ip(700,270)
+score_box.move_ip(700,50)
+
+def draw():
+    global marquee_msg
+    screen.clear()
+    screen.fill("#243E36")
+    screen.draw.filled_rect(marquee_box,"#243E36")
+    screen.draw.filled_rect(question_box,"#222E50")
+    screen.draw.filled_rect(timer_box,"#05A8AA")
+    screen.draw.filled_rect(score_box,"#05A8AA")
+    screen.draw.filled_rect(skip_box,"#2A1E5C")
+
+    for answer_box in answer_boxes:
+        screen.draw.filled_rect(answer_box,"#F25C54")
+
+    marquee_msg = "Welcome to the Quizmaster Show! " + f"You are at..Q: {question_index} of {question_count}!"
+    screen.draw.textbox(marquee_msg,marquee_box,color = "#F7B267")
+    screen.draw.textbox(str(time_left),timer_box,color = "#19180A",shadow = (0.5,0.5),scolor = "grey")
+    screen.draw.textbox("Skip",skip_box,color = "#1F0322",angle =-90)
+    screen.draw.textbox(f"Score: {score}",score_box,color = "#42033D")
+    screen.draw.textbox(question[0].strip(),question_box,color = "#3DFAFF",shadow = (0.5,0.5),scolor = "#1F0322")
+
+
+def update():
+    pass
+
+pgzrun.go()
