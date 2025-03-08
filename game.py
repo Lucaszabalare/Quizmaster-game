43d881c616b96f6fbae26f6d2161d7182ae1cfdd
Lucaszabalare <nellymarac@gmail.com>
2025-03-08 11:24:54 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -21,7 +21,7 @@ questions = []
 question_count = 0
 question_index = 0
 answer_boxes = [answer_box1,answer_box2,answer_box3,answer_box4]
-game_over = False
+is_game_over = False
 marquee_msg = ""
 
 marquee_box.move_ip(0,0)
@@ -53,9 +53,46 @@ def draw():
     screen.draw.textbox("Skip",skip_box,color = "#1F0322",angle =-90)
     screen.draw.textbox(f"Score: {score}",score_box,color = "#42033D")
     screen.draw.textbox(question[0].strip(),question_box,color = "#3DFAFF",shadow = (0.5,0.5),scolor = "#1F0322")
-
+    
+    index = 1
+    for answer_box in answer_boxes:
+        screen.draw.textbox(question[index].strip(),answer_box,color = "#440381")
+        index = index + 1
 
 def update():
-    pass
+    move_marquee()
+
+def move_marquee():
+    marquee_box.x = marquee_box.x - 2
+    if marquee_box.right < 0:
+        marquee_box.left = WIDTH
+
+def read_question_file():
+    global questions, question_count
+    q_file = open(question_file,"r")
+    for question in q_file:
+        questions.append(question)
+        question_count = question_count + 1
+    q_file.close()
+
+def read_next_question():
+    global question_index
+    question_index = question_index + 1
+    return questions.pop(0).split(",")
+
+def on_mouse_down(pos):
+    index = 1
+    for answer_box in answer_boxes:
+        if answer_box.collidepoint(pos):
+            if index == int(question[5]):
+                correct_answer()
+            else:
+                game_over()
+        index = index + 1
+    if skip_box.collidepoint(pos):
+        skip_question()
+
+
+
 
 pgzrun.go()
